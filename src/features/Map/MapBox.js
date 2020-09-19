5a3524dd01d4ed348631bc4d9e6489c2051a1922
Erwan Béguin <erwan.beguin@gmail.com>
2020-09-19 12:16:21 +0200

    ign

--- src/features/Map/MapBox.js
+++ src/features/Map/MapBox.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import "./MapBox.css";
-import { Box, Text } from "grommet";
+import { Box, Button, Text } from "grommet";
 
 const MapBox = () => {
   const [data, setData] = React.useState([]);
+  const [tile, setTile] = React.useState("osm");
 
   React.useEffect(() => {
     fetch(
@@ -16,13 +17,22 @@ const MapBox = () => {
 
   return (
     <Box height="medium" width="large" margin="small">
-      <Map center={[48.8, 2.33]} zoom={11}>
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <Box margin="small" direction="row" gap="small" align="center">
+        <Button onClick={() => setTile("osm")} label="OSM"></Button>
+        <Button onClick={() => setTile("ign")} label="IGN"></Button>
+      </Box>
+      <Map center={[48.847, 2.343]} zoom={12}>
+        {tile === "osm" ? (
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        ) : (
+          <TileLayer url="https://wxs.ign.fr/pratique/geoportail/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&TILEMATRIXSET=PM&TILEMATRIX={z}&TILECOL={x}&TILEROW={y}&STYLE=normal&FORMAT=image/jpeg" />
+        )}
 
         {data.map((item) => (
           <Marker position={item.fields.geo_point_2d}>
             <Popup>
               <Box margin="small" overflow="scroll" height="small">
+                <Text size="small">{item.fields.geo_point_2d}</Text>
                 <Text size="small">{item.fields.synthese}</Text>
               </Box>
             </Popup>
