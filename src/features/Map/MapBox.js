58d3db0620305805f51c4be003c02d563c6c7ad3
Erwan Béguin <erwan.beguin@gmail.com>
2020-09-19 15:57:28 +0200

    ign2

--- src/features/Map/MapBox.js
+++ src/features/Map/MapBox.js
@@ -2,10 +2,13 @@ import React from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import "./MapBox.css";
 import { Box, Button, Text } from "grommet";
+import { exportComponentAsPNG } from "react-component-export-image";
 
 const MapBox = () => {
   const [data, setData] = React.useState([]);
   const [tile, setTile] = React.useState("osm");
+
+  const componentRef = React.useRef();
 
   React.useEffect(() => {
     fetch(
@@ -16,16 +19,25 @@ const MapBox = () => {
   }, []);
 
   return (
-    <Box height="medium" width="large" margin="small">
+    <Box height="medium" width="large" margin="small" align="center">
       <Box margin="small" direction="row" gap="small" align="center">
         <Button onClick={() => setTile("osm")} label="OSM"></Button>
-        <Button onClick={() => setTile("ign")} label="IGN"></Button>
+        <Button onClick={() => setTile("ign")} label="Vue aérienne"></Button>
+        <Button onClick={() => setTile("map")} label="Carte IGN"></Button>
       </Box>
-      <Map center={[48.847, 2.343]} zoom={12}>
+      <Map
+        center={[48.847, 2.343]}
+        zoom={12}
+        ref={componentRef}
+        zoomControl={false}
+        attributionControl={false}
+      >
         {tile === "osm" ? (
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        ) : (
+        ) : tile === "ign" ? (
           <TileLayer url="https://wxs.ign.fr/pratique/geoportail/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&TILEMATRIXSET=PM&TILEMATRIX={z}&TILECOL={x}&TILEROW={y}&STYLE=normal&FORMAT=image/jpeg" />
+        ) : (
+          <TileLayer url="https://wxs.ign.fr/pratique/geoportail/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS&TILEMATRIXSET=PM&TILEMATRIX={z}&TILECOL={x}&TILEROW={y}&STYLE=normal&FORMAT=image/jpeg" />
         )}
 
         {data.map((item) => (
@@ -39,6 +51,12 @@ const MapBox = () => {
           </Marker>
         ))}
       </Map>
+      <Box margin="small" width="small">
+        <Button
+          onClick={() => exportComponentAsPNG(componentRef)}
+          label="Exporter l'image"
+        ></Button>
+      </Box>
     </Box>
   );
 };
