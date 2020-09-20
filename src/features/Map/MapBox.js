f2635f8142609d71cd1311efa524824ecbc46bbb
Erwan Béguin <erwan.beguin@gmail.com>
2020-09-20 12:34:25 +0200

    slider

--- src/features/Map/MapBox.js
+++ src/features/Map/MapBox.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import "./MapBox.css";
-import { Box, Button, Text } from "grommet";
+import { Box, Button, RangeInput, Text } from "grommet";
 import { exportComponentAsPNG } from "react-component-export-image";
 
 const MapBox = () => {
   const [data, setData] = React.useState([]);
   const [tile, setTile] = React.useState("osm");
+  const [composing, setComposing] = React.useState(50);
 
   const componentRef = React.useRef();
 
@@ -24,6 +25,7 @@ const MapBox = () => {
         <Button onClick={() => setTile("osm")} label="OSM"></Button>
         <Button onClick={() => setTile("ign")} label="Vue aérienne"></Button>
         <Button onClick={() => setTile("map")} label="Carte IGN"></Button>
+        <Button onClick={() => setTile("test")} label="Mix Aéro/IGN"></Button>
       </Box>
       <Map
         center={[48.847, 2.343]}
@@ -36,8 +38,20 @@ const MapBox = () => {
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         ) : tile === "ign" ? (
           <TileLayer url="https://wxs.ign.fr/pratique/geoportail/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&TILEMATRIXSET=PM&TILEMATRIX={z}&TILECOL={x}&TILEROW={y}&STYLE=normal&FORMAT=image/jpeg" />
-        ) : (
+        ) : tile === "map" ? (
           <TileLayer url="https://wxs.ign.fr/pratique/geoportail/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS&TILEMATRIXSET=PM&TILEMATRIX={z}&TILECOL={x}&TILEROW={y}&STYLE=normal&FORMAT=image/jpeg" />
+        ) : (
+          <>
+            <TileLayer
+              url="https://wxs.ign.fr/pratique/geoportail/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&TILEMATRIXSET=PM&TILEMATRIX={z}&TILECOL={x}&TILEROW={y}&STYLE=normal&FORMAT=image/jpeg"
+              opacity={composing / 100}
+              zIndex={2}
+            />
+            <TileLayer
+              url="https://wxs.ign.fr/pratique/geoportail/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=GEOGRAPHICALGRIDSYSTEMS.MAPS&TILEMATRIXSET=PM&TILEMATRIX={z}&TILECOL={x}&TILEROW={y}&STYLE=normal&FORMAT=image/jpeg"
+              zIndex={1}
+            />
+          </>
         )}
 
         {data.map((item) => (
@@ -51,7 +65,16 @@ const MapBox = () => {
           </Marker>
         ))}
       </Map>
-      <Box margin="small" width="small">
+      {tile === "test" && (
+        <Box margin="small" gap="small" align="center">
+          <Text>Opacité</Text>
+          <RangeInput
+            value={composing}
+            onChange={(e) => setComposing(e.target.value)}
+          />
+        </Box>
+      )}
+      <Box margin="small" width="small" gap="small">
         <Button
           onClick={() => exportComponentAsPNG(componentRef)}
           label="Exporter l'image"
